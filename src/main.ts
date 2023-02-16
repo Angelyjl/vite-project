86c5e335c304508435c6518536238b958d671bdc
于佳琳 <yujialin@MacBook.local>
2023-02-16 10:41:45 +0800

    封装 axios 网络请求

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 
+import '@/styles/tailwind.css'
+import '@/styles/index.css'
+
 const app = createApp(App)
 // 5. 创建并挂载根实例
 app.use(router)
